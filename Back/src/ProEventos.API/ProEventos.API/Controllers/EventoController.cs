592fb1cd6859f82af728724902301fda3998156c
Bianca <biancalbarbosa@hotmail.com>
2023-11-01 22:57:39 -0300

    Criação da model e controller para Evento

---
+++ Back/src/ProEventos.API/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return string.Empty;
+
+        }
+    }
+}
---
+++ Back/src/ProEventos.API/ProEventos.API/Models/Evento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.API.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public int Local { get; set; }
+        public int DataEvento { get; set; }
+        public int Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public int Lote { get; set; }
+        public int ImagemURL { get; set; }
+    }
+}
