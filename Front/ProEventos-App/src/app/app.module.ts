88417e28d74e00c95c6d826cbf3022de57384230
Bianca <biancalbarbosa@hotmail.com>
2023-11-02 19:55:06 -0300

    Criação do componente palestrantes

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventosComponent
+    EventosComponent,
+    PalestrantesComponent
   ],
   imports: [
     BrowserModule,
