24fa42e1491fca77de6cca1b8b8b9d54da14e99d
Bianca <biancalbarbosa@hotmail.com>
2023-11-02 22:33:02 -0300

    Organizando arquivos 2 + início de getEventos

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public url: string = 'https://localhost:44366/api/eventos';
+  public eventos: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    // this.http.get(this.url).subscribe(
+    //   response => this.eventos = response,
+    //   error => console.log(error)
+    // );
+
+
+
+    this.eventos = [
+      {
+        Tema: 'Evento A',
+        Local: ''
+      },
+      {
+        Tema: 'Evento B',
+        Local: ''
+      },
+      {
+        Tema: 'Evento C',
+        Local: ''
+      },
+    ]
+  }
+}
